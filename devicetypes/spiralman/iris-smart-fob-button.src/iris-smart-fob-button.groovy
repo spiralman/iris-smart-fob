2070f04783ff535914e12dd22c4fa9384a6e6fee
Thomas Stephens <spiralman@gmail.com>
2019-02-02 13:35:09 -0600

    Trying to get the button presses to work

--- devicetypes/spiralman/iris-smart-fob-button.src/iris-smart-fob-button.groovy
+++ devicetypes/spiralman/iris-smart-fob-button.src/iris-smart-fob-button.groovy
@@ -45,10 +45,10 @@ def buttonUp() {
 
 private pushed() {
   log.debug "${device.label} pushed"
-  sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1])
+  sendEvent(name: "Button", value: "pushed", data: [buttonNumber: 1])
 }
 
 private held() {
   log.debug "${device.label} held"
-  sendEvent(name: "button", value: "held", data: [buttonNumber: 1])
+  sendEvent(name: "Button", value: "held", data: [buttonNumber: 1])
 }
